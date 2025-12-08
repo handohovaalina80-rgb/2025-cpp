1be2904fd411df9ffca5eefadcd92a0338754dd6
handohovaalina80-rgb <handohovaalina80@gmail.com>
2025-12-09 01:10:27 +0300

    Update source.cpp

---
+++ Homework-1/Project8/source.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(int argc, char** argv)
+{
+    int M, T;
+    scanf("%d %d", &M, &T);
+    int R = T - M;
+    if (R < M) 
+    {
+        printf("Ivan");
+    } else 
+    {
+        printf("Ivan");
+    }
+    
+    return 0;
+}
